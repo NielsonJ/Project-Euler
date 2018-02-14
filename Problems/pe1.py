ecd04807fa58a132a28228b7e34a2f174ff6ede6
Nielson Janné <nielsonj@gmail.com>
2018-02-14 20:11:32 +0100

    pe2 finished

--- Problems/pe1.py
+++ Problems/pe1.py
@@ -1,13 +1,8 @@
+import time as time
 
 # Question:
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
-#
-# ToDo:
-# Find all multiplications of 3 below 1000
-# Find all multiplications of 5 below 1000
-# Add sum
-import time as time
 
 LIMIT = 1000
 
