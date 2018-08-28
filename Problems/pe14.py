3601efc0e26da0a26082c6ab7586889d8f1c40b9
Nielson Janné <nielsonj@gmail.com>
2018-08-28 16:09:15 +0200

    pe14 optimization accepted

--- Problems/pe14.py
+++ Problems/pe14.py
@@ -5,31 +5,26 @@ def main():
     print('script started ...')
     start = time.clock()
 
-    answer = getAnswer()
+    answer = (1, 1)
+    collatzDict = {1: 1}
+    for startNumber in range(1, 1000000):
+        NumberToCalc = startNumber
+        chain = 0
+        while NumberToCalc not in collatzDict:
+            chain += 1
+            if NumberToCalc % 2 == 0:
+                NumberToCalc = NumberToCalc / 2
+            else:
+                NumberToCalc = 3 * NumberToCalc + 1
+        temp = collatzDict[NumberToCalc] + chain
+        collatzDict[startNumber] = temp
+        print("[ " + str(startNumber) + " ] chain: " + str(temp))
+        if answer[1] < temp:
+            answer = (startNumber, temp)
 
     end = time.clock()
     print('answer: ' + str(answer))
     print('time: ' + str(end - start))
-
-
-def getAnswer():
-    answer = 0
-    answer_chain = 0
-    for a in range(1, 1000000):
-        n = a
-        chain = 1
-        while n != 1:
-            if n % 2 == 0:
-                n = n / 2
-            else:
-                n = 3 * n + 1
-            chain += 1
-        if chain > answer_chain:
-            answer = a
-            answer_chain = chain
-        print("[ " + str(a) + " ]  answer: " + str(answer) +
-              "\t answer-chain: " + str(answer_chain))
-    return answer
 
 
 if __name__ == '__main__':
