f9319d54e7d2870aee0c7ffdecbd3630b33d8cf8
Nielson Janné <nielsonj@gmail.com>
2018-09-02 11:14:31 +0200

    pe14 optimization

--- Problems/pe14.py
+++ Problems/pe14.py
@@ -6,7 +6,7 @@ def main():
     start = time.clock()
 
     answer = (1, 1)
-    collatzDict = {1: 1}
+    collatzDict = {1: 1}  # don't recalc
     for startNumber in range(1, 1000000):
         NumberToCalc = startNumber
         chain = 0
@@ -16,11 +16,11 @@ def main():
                 NumberToCalc = NumberToCalc / 2
             else:
                 NumberToCalc = 3 * NumberToCalc + 1
-        temp = collatzDict[NumberToCalc] + chain
-        collatzDict[startNumber] = temp
-        print("[ " + str(startNumber) + " ] chain: " + str(temp))
-        if answer[1] < temp:
-            answer = (startNumber, temp)
+        collatzDict[startNumber] = collatzDict[NumberToCalc] + chain
+        print("[ " + str(startNumber) + " ] chain: " +
+              str(collatzDict[startNumber]))
+        if answer[1] < collatzDict[startNumber]:
+            answer = (startNumber, collatzDict[startNumber])
 
     end = time.clock()
     print('answer: ' + str(answer))
