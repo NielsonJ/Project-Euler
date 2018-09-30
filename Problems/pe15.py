e46ecd5373f3999909a53a184ca0815808a95760
Nielson Janné <nielsonj@gmail.com>
2018-09-30 20:07:12 +0200

    implemented pe15

--- Problems/pe15.py
+++ Problems/pe15.py
@@ -10,15 +10,25 @@ import math
 def main():
     print('script started ...')
     start = time.perf_counter()
-    answer = function()
+    answer = function(20)
     end = time.perf_counter()
     print('answer: ' + str(answer))
     print('time: ' + str(end - start))
 
 
-def function():
-
-    return
+def function(gridsize):
+    gridsize += 1  # add one to gridsize to represent each node.
+    oldgrid = [1]
+    for x in range(0, gridsize):
+        newgrid = [1]
+        for y in range(0, x):
+            if ((y + 1) == x):
+                newgrid.append(newgrid[y] * 2)
+            else:
+                newgrid.append(newgrid[y] + oldgrid[y + 1])
+        # print(newgrid)
+        oldgrid = newgrid
+    return oldgrid[-1]
 
 
 if __name__ == '__main__':
